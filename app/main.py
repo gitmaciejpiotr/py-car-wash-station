e94939b55d634feebbc2c0b77d9206aaab913435
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-01-09 17:24:24 +0100

    fixed last flake8 and semantic errors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Car:
     # write your code here
-    def __init__(self, comfort_class: int, clean_mark: int, brand: int) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -8,8 +8,8 @@ class Car:
 
 class CarWashStation:
     # write your code here
-    def __init__(self, distance_from_city_center: int, clean_power: int,
-                 average_rating: int, count_of_ratings: int) -> None:
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -34,5 +34,5 @@ class CarWashStation:
 
     def rate_service(self, rate: int) -> None:
         self.average_rating = round((self.average_rating * self.count_of_ratings + rate)
-                                    / (self.count_of_ratings + 1))
+                                    / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
