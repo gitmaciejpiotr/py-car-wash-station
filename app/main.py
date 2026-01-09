e7f4c8f2dcf0ca65def80beb8a2827201da3891a
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-01-09 17:48:16 +0100

    removed necessery logic to wash_single_car method and fixed operations in serve_cars

--- app/main.py
+++ app/main.py
@@ -14,22 +14,24 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> int:
+    def serve_cars(self, cars: list) -> float:
         income = 0
 
         for car in cars:
-            if car.clean_mark < self.clean_power:
-                income += self.calculate_washing_price(car)
-                car.clean_mark = self.wash_single_car(car)
+            income += self.calculate_washing_price(car)
+            car.clean_mark = self.wash_single_car(car)
 
-        return income
+        return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center, 1)
+        return (car.comfort_class * (self.clean_power - car.clean_mark)
+                * self.average_rating / self.distance_from_city_center)
 
     def wash_single_car(self, car: Car) -> int:
-        return self.clean_power
+        if car.clean_mark < self.clean_power:
+            return self.clean_power
+        else:
+            return car.clean_mark
 
     def rate_service(self, rate: int) -> None:
         self.average_rating = round((self.average_rating
