50765190a9adee3360b45a8b7989a6d83a2ab72c
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-01-09 17:21:06 +0100

    fixed another flake8 errors

--- app/main.py
+++ app/main.py
@@ -5,15 +5,17 @@ class Car:
         self.clean_mark = clean_mark
         self.brand = brand
 
+
 class CarWashStation:
     # write your code here
-    def __init__(self, distance_from_city_center: int, clean_power: int, average_rating: int, count_of_ratings: int) -> None:
+    def __init__(self, distance_from_city_center: int, clean_power: int,
+                 average_rating: int, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars) -> int:
+    def serve_cars(self, cars: list) -> int:
         income = 0
 
         for car in cars:
@@ -23,12 +25,14 @@ class CarWashStation:
 
         return income
 
-    def calculate_washing_price(self, car) -> int:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center, 1)
+    def calculate_washing_price(self, car: Car) -> float:
+        return round(car.comfort_class * (self.clean_power - car.clean_mark) *
+                     self.average_rating / self.distance_from_city_center, 1)
 
-    def wash_single_car(self, car) -> int:
+    def wash_single_car(self, car: Car) -> int:
             return self.clean_power
 
-    def rate_service(self, rate) -> None:
-        self.average_rating = round((self.average_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1))
+    def rate_service(self, rate: int) -> None:
+        self.average_rating = round((self.average_rating * self.count_of_ratings + rate)
+                                    / (self.count_of_ratings + 1))
         self.count_of_ratings += 1
